dab1851c60b414df8c0db65a68e343933519150c
Christopher Miller <cmiller@lta00305jfk.local>
2021-02-09 13:06:23 -0500

    update to accept files

--- index.js
+++ index.js
@@ -1,15 +1,33 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const fs = require('fs');
 const { Pool } = require('pg')
 const pool = new Pool();
 
 try{
-    pool.query(process.env.SCRIPT, (err, res) => {
-        core.setOutput("psql", JSON.stringify(res));
-        pool.end()
+    fs.readFile(process.env.SCRIPT, 'utf8', function(err, data) {
+        if (err) throw err;
+        const query = data;
+        console.log("here is the query \n", query);
+
+        pool.query(query, (err, res) => {
+            core.setOutput("psql", JSON.stringify(res));
+            pool.end()
       });
+    });
 }
 catch(error){
     core.setOutput("psql", error);
     core.setFailed(error.message);
 }
+
+
+
+// TOOD:
+
+
+// Accept multiple files
+
+// Schemas always run first
+
+
